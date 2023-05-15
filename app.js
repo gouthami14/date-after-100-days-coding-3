c1b5b1a5ad7fbed82406f9bd3e811317b752dbc0
gouthami14 <92106741+gouthami14@users.noreply.github.com>
2023-05-15 17:36:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 app.get("/",(request, response) => {
     const dateTime = new Date();
     const result = addDays(
-        new Date(dateTime.getFullYear(), dateTime.getMonth(), dateTime.getDate(),
+        new Date(dateTime.getFullYear(), dateTime.getMonth(), dateTime.getDate()),
         100
     );
     response.send(
